12e8505a20bb9da3a18ad6714411a22c6c1a00fd
Rahul <147621593+Rahul99699@users.noreply.github.com>
2026-04-17 17:26:03 +0530

    Delete test_model.py

--- test_model.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-model = joblib.load("sentiment_model.pkl")
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
-
-text = ["this product is very bad"]
-vec = vectorizer.transform(text)
-
-print(model.predict(vec))
